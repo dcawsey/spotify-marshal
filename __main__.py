7b26bf13fe0ce3901bf5c395e7acc8b9f1d17942
Dylan Cawsey <dylancawsey@gmail.com>
2024-03-17 10:47:46 +1100

    Working auth for endpoint that does not access user data - need different auth type

--- __main__.py
+++ __main__.py
@@ -19,18 +19,42 @@ basic_auth = f"{client_id}:{client_secret}"
 basic_auth_bytes = str.encode(basic_auth)
 auth_b64encoded_bytes = b64encode(basic_auth_bytes)
 
+# TODO: may be worth using an OAuth SDK
 auth_response = requests.post(
     "https://accounts.spotify.com/api/token",
     headers={
         "Content-Type": "application/x-www-form-urlencoded",
         "Authorization": f"Basic {auth_b64encoded_bytes.decode()}",
     },
-    data={"grant_type": "client_credentials"},
+    data={
+        "grant_type": "client_credentials"
+    },  #! this auth type is only valid for server to server -> cannot use any APIs for user data
 )
 
 if auth_response.status_code != 200:
-    sys.exit(f"Authentication failed: {auth_response.json()}")
+    sys.exit(f"ERROR: Authentication failed - {auth_response}")
 
-response_body = auth_response.json()
+auth_response_body = auth_response.json()
 
-access_token = response_body.get("access_token")
+access_token = auth_response_body.get("access_token")
+
+search_query_response = requests.get(
+    "https://api.spotify.com/v1/search",
+    params={
+        "limit": 10,
+        "type": "track",
+        "q": "King Kunta",
+    },
+    headers={
+        "Authorization": f"Bearer {access_token}",
+    },
+)
+
+if search_query_response.status_code != 200:
+    sys.exit(
+        f"ERROR: GET user's top tracks request failed - {search_query_response.json()}"
+    )
+
+search_query_response_body = search_query_response.json()
+
+print("search_query_response_body", search_query_response_body)
